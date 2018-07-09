ad170fed89f0e65b00761d34fa41ea5ee9b71188
Tom <32444267+dovantuit@users.noreply.github.com>
2018-07-10 00:03:54 +0700

    Update tom.js

--- tom.js
+++ tom.js
@@ -1,25 +1,34 @@
 let x = Math.floor(Math.random()* 10);
-function checknumber(x){
-  
+function checkdouble(x,arrayx){
+  for( let i = 0; i< arrayx.length;i++){
+    if(x == arrayx[i])
+      return "you guessed this number already!";
+  }
+  return true;
 };
-let count = true;
-let arrayList = [];
+let count = 10;
+let pastGuesses = []; 
 
   
     for(let i = 0; i<10; i++){
       let y = prompt("what number do you think?");
-    arrayList.push(y);
-     if(x == y)
+    pastGuesses.push(y);
+      if(checkdouble(y,pasGuessed)==true){
+        if(x == y)
      {
        alert("you win");
      }
      if(x<y){
-       alert('you numer '+y+' too high. Your previous number: '+arrayList);
+       count--;
+       alert('you guesses of '+y+' was too high.You have '+ count+' guesses remaining. Your previous number: '+pastGuesses);
        
      }
      if(x>y){
-       alert('you numer '+y+' too low. Your previous number: '+arrayList);
+       count--;
+       alert('you guesses of '+y+' was too low. You have '+ count+' guesses remaining. Your previous number: '+pastGuesses);
      }
+      }
+     
     }
     
       
